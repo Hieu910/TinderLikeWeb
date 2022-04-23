e73ba71aa2ff294d8d28252a166af27b8a2f632f
unknown <handsomebanana910@gmail.com>
2022-04-23 19:01:48 +0700

    add _redirects to public folder

--- src/components/Map.js
+++ src/components/Map.js
@@ -48,7 +48,7 @@ const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherDat
         ))}
         {weatherData?.list?.length && weatherData.list.map((data, i) => (
           <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
-            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+            <img alt="" src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
           </div>
         ))}
       </GoogleMapReact>
